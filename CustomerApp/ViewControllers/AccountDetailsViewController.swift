58822523fc1f07752a3d9f0e552fa0c4e27cacdd
Eeswar Batchu <ebat0007@student.monash.edu>
2020-11-15 15:21:58 +1100

    commit15

--- CustomerApp/ViewControllers/AccountDetailsViewController.swift
+++ CustomerApp/ViewControllers/AccountDetailsViewController.swift
@@ -9,15 +9,39 @@ import Foundation
 import UIKit
 
 
-class AccountDetailsViewController:UIViewController{
+class AccountDetailsViewController:UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var accountDetailsTableView: UITableView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        attachDelegate()
     }
-    ///////////
-    /////////////
+    
+    func attachDelegate(){
+        
+        accountDetailsTableView.delegate = self
+        accountDetailsTableView.dataSource = self
+        
+        
+    }
     
     
-    ///////////////////////////////
-    ////////////////////////////
-}
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+    }
+    
+    
+    
+    }
